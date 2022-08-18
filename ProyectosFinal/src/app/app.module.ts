885db762148128b36e4a4661c0cb12bf0bd68897
jpGonzalez23 <juanpigonzalez72@gmail.com>
2022-08-18 16:09:43 -0300

    Agregamos el header

--- ProyectosFinal/src/app/app.module.ts
+++ ProyectosFinal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedSocialComponent } from './components/red-social/red-social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedSocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
