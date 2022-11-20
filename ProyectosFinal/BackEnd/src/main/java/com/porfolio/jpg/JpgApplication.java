d6bdbd8d0b4d89bc012a7f4764d20bbedfea3215
jpGonzalez23 <juanpigonzalez72@gmail.com>
2022-11-20 17:00:05 -0300

    Modificaciones

---
+++ ProyectosFinal/BackEnd/src/main/java/com/porfolio/jpg/JpgApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.jpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpgApplication.class, args);
+	}
+
+}
---
+++ ProyectosFinal/BackEnd/src/test/java/com/porfolio/jpg/JpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.jpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
